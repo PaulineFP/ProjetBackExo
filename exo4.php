5704d999b2c5c454c1af8deba405478b464bd7b8
Pauline <pauline.frare-pradal@laposte.net>
2020-11-09 11:26:57 +0100

    debut

--- exo4.php
+++ exo4.php
@@ -1,3 +1,9 @@
 <?php
 //Obtenir le nom des 10 villes les plus peuplées en 2012, ainsi que le nom du département associé
+
+//je me co a ma bdd
+// je crée une requette pour indiquer les selections avec restrictions
+//j'affiche
+
+//---------------------------------------------------------------------------------------------------------------
 ?>
