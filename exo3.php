9cb196d908588d40c6f31c222f1cca35e907bce9
Pauline <pauline.frare-pradal@laposte.net>
2020-11-09 10:44:42 +0100

    Update exo3.php

--- exo3.php
+++ exo3.php
@@ -1,3 +1,11 @@
 <?php
 // Obtenir la liste des départements d’outres-mer, c’est-à-dire ceux dont le numéro de département commencent par “97”
+
+//Je me connecte a ma bdd
+//Crée une requette pour indicquer la selection
+//Faire le tri
+// Afficher
+//----------------------------------------------------------------------------------------------------------------------
+
+
 ?>
