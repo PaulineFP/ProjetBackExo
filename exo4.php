c15e3cceb0459b6eced07b130a9d55373ca1ec2d
Pauline <pauline.frare-pradal@laposte.net>
2020-11-09 11:45:16 +0100

    fini

--- exo4.php
+++ exo4.php
@@ -6,4 +6,21 @@
 //j'affiche
 
 //---------------------------------------------------------------------------------------------------------------
+
+//je me connect à ma bdd
+$pdo = new PDO('mysql:host=mysql;dbname=cataloguevillesfr;host=127.0.0.1', 'root', '', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+// je crée une requette pour indiquer les selections avec restrictions
+$pdostat = $pdo->query("SELECT * FROM villes_france_free ORDER BY ville_population_2012 DESC LIMIT 10");
+
+//j'affiche
+
+while ($donnee = $pdostat->fetch())
+{
+    echo $donnee['ville_nom']. 'a ';
+    echo $donnee['ville_population_2012']. 'habitants. Elle se situe dans le département ';
+    echo $donnee['ville_departement']. ' . ';
+}
 ?>
