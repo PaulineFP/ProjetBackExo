36d8796f2f90429e25ca0249b28d12bef78eda0a
Pauline <pauline.frare-pradal@laposte.net>
2020-11-09 10:24:31 +0100

    Fini

--- exo2.php
+++ exo2.php
@@ -12,9 +12,10 @@ $pdo = new PDO('mysql:host=mysql;dbname=cataloguevillesfr;host=127.0.0.1', 'root
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
-// je crée une requette en lui demandant de ranger dans en ordre de surface décroissant.
+/* je crée une requette, et je lui demande de ranger les infos dans en ordre de surface décroissant,
+puis d'afficher que les 50 premiers de la liste*/
 
-$pdostat = $pdo->query("SELECT * FROM villes_france_free ORDER BY ville_surface DESC " );
+$pdostat = $pdo->query("SELECT * FROM villes_france_free ORDER BY ville_surface DESC LIMIT 50" );
 
 
 //j'affiche
@@ -24,8 +25,6 @@ while ($donnee = $pdostat->fetch())
    echo $donnee['ville_nom'];
    echo $donnee['ville_surface'];
 }
-
-
 
 
 ?>
